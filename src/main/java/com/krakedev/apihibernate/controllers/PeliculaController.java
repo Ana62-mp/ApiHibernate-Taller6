8fea8518ad4e5405096c76575d25112f18995a0b
Ana62-mp <ana.moposita84@gmail.com>
2026-05-21 21:43:46 -0500

    APMP: creacion de controller y metodos

---
+++ src/main/java/com/krakedev/apihibernate/controllers/PeliculaController.java
@@ -0,0 +1,66 @@
+package com.krakedev.apihibernate.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.krakedev.apihibernate.entidades.Pelicula;
+import com.krakedev.apihibernate.services.ServicioPelicula;
+
+@RestController
+@RequestMapping("/peliculas")
+public class PeliculaController {
+	private final ServicioPelicula servicioP;
+
+	public PeliculaController(ServicioPelicula servicioP) {
+		super();
+		this.servicioP = servicioP;
+	}
+	
+	@PostMapping
+	public Pelicula crear(@RequestBody Pelicula pelicula) {
+		return servicioP.crear(pelicula);
+	}
+	
+	@GetMapping
+	public List<Pelicula> listar(){
+		return servicioP.listar();
+	}
+	
+	@GetMapping("/{id}")
+	public Pelicula buscarPorId(@PathVariable Long id) {
+		return servicioP.buscarPorId(id);
+	}
+	
+	@PutMapping("/{id}")
+	public Pelicula actualizar(@PathVariable Long id, @RequestBody Pelicula pelicula) {
+		return servicioP.actualizar(id, pelicula);
+	}
+	
+	@DeleteMapping("/{id}")
+	public boolean eliminar(@PathVariable Long id) {
+		return servicioP.eliminar(id);
+	}
+	
+	@GetMapping("/{genero}")
+	public List<Pelicula> buscarPorGenero(@PathVariable String genero){
+		return servicioP.buscarPorGenero(genero);
+	}
+	
+	@GetMapping("/{disponible}")
+	public List<Pelicula> buscarPorDisponible(@PathVariable boolean disponible){
+		return servicioP.buscarPorDisponible(disponible);
+	}
+	
+	
+	
+	
+	
+}
