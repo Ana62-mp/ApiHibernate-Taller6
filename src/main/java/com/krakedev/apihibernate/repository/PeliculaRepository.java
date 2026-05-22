96dcd374a235ccb5a728acfa2f8e156294770563
Ana62-mp <ana.moposita84@gmail.com>
2026-05-21 21:44:43 -0500

    APMP: agregue query personalizado en repository

--- src/main/java/com/krakedev/apihibernate/repository/PeliculaRepository.java
+++ src/main/java/com/krakedev/apihibernate/repository/PeliculaRepository.java
@@ -3,6 +3,8 @@ package com.krakedev.apihibernate.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.krakedev.apihibernate.entidades.Pelicula;
 
@@ -13,4 +15,7 @@ public interface PeliculaRepository extends JpaRepository<Pelicula, Long>{
 	List<Pelicula> findByDisponible(boolean disponible);
 	List<Pelicula> findByDirector(String director);
 	
+	//Para casos especiales si tenemos que hacer query
+	@Query("SELECT p FROM Pelicula p WHERE p.genero = :genero AND p.duracion < :duracion")
+	List<Pelicula> findByGeneroYDuracionMenorA(@Param("genero") String genero, @Param("duracion") int duracion);
 }
