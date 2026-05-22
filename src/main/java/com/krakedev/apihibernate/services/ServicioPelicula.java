a6e9c7d5ad01dcbc9fafeb25a1533ca98e1232f6
Ana62-mp <ana.moposita84@gmail.com>
2026-05-21 21:13:38 -0500

    APMP: desarrollo de servicio, implementar metodo crear, listar, BuscarPorId

--- src/main/java/com/krakedev/apihibernate/services/ServicioPelicula.java
+++ src/main/java/com/krakedev/apihibernate/services/ServicioPelicula.java
@@ -1,8 +1,37 @@
 package com.krakedev.apihibernate.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.krakedev.apihibernate.entidades.Pelicula;
+import com.krakedev.apihibernate.repository.PeliculaRepository;
 
 @Service
 public class ServicioPelicula {
+	private final PeliculaRepository peliculaRepository;
 
+	public ServicioPelicula(PeliculaRepository peliculaRepository) {
+		super();
+		this.peliculaRepository = peliculaRepository;
+	}
+	
+	public Pelicula crear(Pelicula pelicula) {
+		//Save para guardar y acatualizar
+		Pelicula pNueva = peliculaRepository.save(pelicula);
+		return pNueva;
+	}
+	
+	public List<Pelicula> listar(){
+		return peliculaRepository.findAll();
+	}
+	
+	public Pelicula buscarPorId(Long id) {
+		// metodo find by id es siempre para buscar primary key, incluso si se llama cedula es findbyid
+		return peliculaRepository.findById(id).orElse(null);
+	}
+	
+	
+	
+	
 }
